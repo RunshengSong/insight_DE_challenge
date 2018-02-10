1514864ec9513e0f77c5c827bac5c728ac732a36
Runsheng Song <runsheng@umail.ucsb.edu>
2018-02-09 16:57:52 -0800

    init

---
+++ src/donation_analyser.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 17:28:42 2018
+
+@author: runshengsong
+"""
+import helper
+
+import math
+import datetime
+import numpy as np
+
+from collections import defaultdict
+
+class DonationAnalyzer():
+    def __init__(self, input_file_path, input_percentile_file_path, output_file_path):
+        # TO DO
+        # Initialize a database here to record the previous input values
+        # Initialize the percentile value here
+        # should I use pd dataframe for the database or dictionary {(Name, ZIP): [values]}?
+        self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
+        
+        self.repeat_donors, self.records = self.__init_database()
+        with open(input_percentile_file_path, 'rb') as myfile:
+            self.input_percentile = int(myfile.read())
+    
+    def start(self):
+        """
+        start listing the input stream
+        
+        In this case, the input will be in a txt file
+        """
+        with open(self.input_file_path, 'rb') as input_file:
+            with open(self.output_file_path, 'wb') as output_file:
+                for each_input_string in input_file:
+                    this_input_list = each_input_string.split('|')
+                    if helper.is_valid_input(this_input_list):
+                        this_input_list = helper.get_relevant_field(this_input_list)
+                        # check if this streaming in records belongs to a repeat donor
+                        is_repeat_donor = self.__is_repeat(this_input_list)
+    
+                        if is_repeat_donor:
+                            # add to the repeated donor list
+                            self.__hash_to_repeated_donor(this_input_list)
+                            
+                            # yield one result
+                            this_result = self.__yield_results(this_input_list)
+                            
+                            # write to file
+                            output_file.write(this_result + "\n")          
+                        # add to the records database
+                        self.__hash_to_record(this_input_list)
+                    
+    def __init_database(self):
+        """
+        initialize two empty databases to store the data
+        
+        (1) repeat donors: dictionary:
+            (CMTE_ID, ZIP_CODE, YEAR):[[NAME, TRANSACTION_DT, TRANSACTION_AMT]]
+        
+        (2) record: ordered dictionary
+            (NAME, ZIP_CODE): [[CMTE_ID, DT, AMT]]
+        """
+        repeat_donors = defaultdict(list)
+        records = defaultdict(list)
+        return repeat_donors, records
+
+    def __is_repeat(self, input_values):
+        """
+        check if the input values belong to a repeat donor
+        
+        Input: records
+        Output: true or false
+        """
+        this_donor = (input_values[1], input_values[2])
+        
+        print input_values
+        if this_donor in self.records:
+            # check if the Year is eailer than the current year
+            current_donation_date = datetime.datetime.strptime(input_values[3], '%m%d%Y')
+            this_donor_donate_records = self.records[this_donor] # may be multiple
+            
+            # check if all donation years are later than the current year
+            # deal with the non-chronologically streamings....
+            for each_records in this_donor_donate_records:
+                each_donation_date = datetime.datetime.strptime(each_records[1], '%m%d%Y')
+                if current_donation_date >= each_donation_date:
+                    # if the current donation happens 
+                    # after than the recored donation date
+                    # than it is a repeated donar
+                    return True
+            return False
+        else:
+            return False
+
+    
+    def __hash_to_record(self, input_values):
+        """
+        update the records database
+        """
+        this_NAME = input_values[1]
+        this_ZIPCODE = input_values[2]
+        
+        this_CMTE_ID = input_values[0]
+        this_DATE = input_values[3]
+        this_AMT = input_values[4]
+
+        self.records[(this_NAME, this_ZIPCODE)].append([this_CMTE_ID, this_DATE, this_AMT])
+        
+    def __hash_to_repeated_donor(self, input_values):
+        """
+        update the input values to the records database
+        """
+        this_NAME = input_values[1]
+        this_ZIPCODE = input_values[2]
+        this_CMTE_ID = input_values[0]
+        this_DATE = input_values[3]
+        this_AMT = input_values[4]
+        this_Year = datetime.datetime.strptime(this_DATE, '%m%d%Y').year
+        
+        self.repeat_donors[(this_CMTE_ID, this_ZIPCODE, this_Year)].append([this_NAME, this_DATE, this_AMT])
+    
+    def __yield_results(self, input_values):
+        """
+        Generate one output
+        """
+        this_NAME = input_values[1]
+        this_ZIPCODE = input_values[2]
+        this_CMTE_ID = input_values[0]
+        this_DATE = input_values[3]
+        this_AMT = input_values[4]
+        this_Year = datetime.datetime.strptime(this_DATE, '%m%d%Y').year
+        
+        this_repeated_donor_infos = self.repeat_donors[((this_CMTE_ID, this_ZIPCODE, this_Year))]
+        
+        # calculate the sum and the percentile of the input repeat donors
+        this_sum, this_percentile = self.__calculate_sum_and_percentile(this_repeated_donor_infos)
+        
+        return this_CMTE_ID + "|" + this_ZIPCODE + "|" + str(this_Year) + "|" + str(this_sum) + "|" + str(this_percentile) + "|" + str(len(this_repeated_donor_infos))
+
+    def __calculate_sum_and_percentile(self, repeated_donor_infos):
+        """
+        Calculate the summation and percentile of the given repeated donors' information
+        """
+        to_sum_up = []
+        for each_lists in repeated_donor_infos:
+            this_AMT = int(each_lists[-1])
+            to_sum_up.append(this_AMT)
+        
+        return np.sum(to_sum_up), self.__nearest_percentile(to_sum_up, self.input_percentile)
+    
+    def __nearest_percentile(self, amounts, input_percent):
+        """
+        return the percentile for the amounts using the nearest method
+        """
+        sorted(amounts)
+        ordinal_rank = int(math.ceil((input_percent/100.0) * len(amounts)))
+        return amounts[ordinal_rank - 1]
+        
+if __name__ == "__main__":
+    # loading just for testing
+    input_data_file_path = "../input/itcont_test.txt"
+    input_percent_file_path = "../input/percentile.txt"
+    output_file_path = "../output/output_test.txt"
+    
+    this_analyzer = DonationAnalyzer(input_data_file_path, input_percent_file_path, output_file_path)
+    this_analyzer.start()
---
+++ src/helper.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 16:01:45 2018
+
+@author: runshengsong
+"""
+import datetime
+
+def process_input(input_values):
+    """
+    process the 
+    """
+    pass
+
+def has_no_other_ID(input_values):
+    """
+    check if the input_values has no otherID
+    Input: list
+    """
+    return input_values[15] == ""
+
+def has_valid_date(input_values):
+    """
+    check if the input_values has valid date
+    Input: list
+    """
+    date_time = input_values[13]
+    try:
+        datetime.datetime.strptime(date_time, '%m%d%Y')
+        return True
+    except ValueError:
+        return False
+
+def has_valid_zip_code(input_values):
+    """
+    check if the input_values has valid ZIPCODE
+    Input: list
+    """
+    zip_code = input_values[10]
+    return len(zip_code) >= 5
+
+def has_no_other_missing_field(input_values):
+    """
+    check if the input_values has valid ZIPCODE
+    Input: list
+    """
+    CMTE_ID = input_values[0]
+    TRANSACTION_AMT = input_values[14]
+    return not (CMTE_ID == "" or TRANSACTION_AMT == "")
+
+def is_valid_input(input_values):
+    """
+    return True if the input value don't contain a OTHER_ID
+    Otherwise false
+    
+    Input: Input_value, str
+    """
+    return has_no_other_ID(input_values) and \
+           has_valid_date(input_values) and \
+           has_valid_zip_code(input_values) and \
+           has_no_other_missing_field(input_values)
+           
+def get_relevant_field(input_values):
+    """
+    return a list of only relavent information
+    """
+    return [input_values[0], input_values[7], input_values[10][:5], input_values[13], input_values[14], input_values[15]]
+
+
+if __name__ == "__main__":
+    # loading just for testing
+    input_data_file_path = "../input/itcont_test.txt"
+    input_percent_file_path = "../input/percentile.txt"
+    
+    with open(input_data_file_path, 'r') as myfile:
+        for each_line in myfile:
+            this_value = each_line.split("|")
+            print is_valid_input(this_value)
